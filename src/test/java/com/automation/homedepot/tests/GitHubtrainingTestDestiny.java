2a0c1f55873d28e2f0f2516612e56bb5c2d788e5
destinybryant29-eng <destinybryant29@gmail.com>
2025-09-20 11:52:07 -0500

    adding new test

---
+++ src/test/java/com/automation/homedepot/tests/GitHubtrainingTestDestiny.java
@@ -0,0 +1,12 @@
+package com.automation.homedepot.tests;
+
+import org.testng.annotations.Test;
+
+public class GitHubtrainingTestDestiny extends BaseTest{
+
+
+    @Test
+    public void githubTrainingTest(){
+        System.out.println("destiny");
+    }
+}
