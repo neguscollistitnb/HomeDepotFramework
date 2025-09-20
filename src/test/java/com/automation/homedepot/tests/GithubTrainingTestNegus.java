7d5a5c7eccb79176da9f5936f63688e2b6f65f06
neguscollistitnb <neguscollistitnb@gmail.com>
2025-09-20 11:04:21 -0500

    Adding new tests

---
+++ src/test/java/com/automation/homedepot/tests/GithubTrainingTestNegus.java
@@ -0,0 +1,11 @@
+package com.automation.homedepot.tests;
+
+import org.testng.annotations.Test;
+
+public class GithubTrainingTestNegus {
+
+    @Test
+    public void githubTrainingTest(){
+        System.out.println("Github training");
+    }
+}
